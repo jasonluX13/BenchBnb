6f5c0b954cfca7a7a9c9629dc0b1b70891a891c4
Jason Lu <jasonluX13@gmail.com>
2019-09-30 16:19:10 -0400

    Created initial database and started repositories

--- AirBench/Controllers/HomeController.cs
+++ AirBench/Controllers/HomeController.cs
@@ -1,14 +1,25 @@
-﻿using System.Web.Mvc;
+﻿using AirBench.Data;
+using AirBench.Models;
+using AirBench.Repository;
+using System.Collections.Generic;
+using System.Web.Mvc;
 
 namespace AirBench.Controllers
 {
     public class HomeController : Controller
     {
+        private Context context;
+        public HomeController()
+        {
+            context = new Context();
+        }
         public ActionResult Index()
         {
-            ViewBag.Title = "Air Bench";
 
-            return View();
+            ViewBag.Title = "Air Bench";
+            BenchRepository repo = new BenchRepository(context);
+            List<Bench> users = repo.GetAll();
+            return View(users);
         }
     }
 }
---
+++ AirBench/Data/DatabaseInitializer.cs
@@ -0,0 +1,36 @@
+﻿using AirBench.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace AirBench.Data
+{
+    public class DatabaseInitializer : DropCreateDatabaseAlways<Context>
+    {
+        protected override void Seed(Context context)
+        {
+            User jason = new User()
+            {
+                FirstName= "Jason",
+                LastName ="Lu",
+                Email = "jlu@gmail.com",
+                HashedPassword = ""
+            };
+
+            context.Users.Add(jason);
+
+
+            Bench old = new Bench()
+            {
+                Description = "An old bench",
+                User = jason,
+                NumSeats = 4
+            };
+
+            context.Benches.Add(old);
+            context.SaveChanges();
+        }
+    }
+}
