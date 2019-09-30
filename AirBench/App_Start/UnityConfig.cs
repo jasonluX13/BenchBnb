21f096c5dd5141e9cb499a3d5f48aa174977bf35
Jason Lu <jasonluX13@gmail.com>
2019-09-30 11:08:44 -0400

    Initial project files and packages

---
+++ AirBench/App_Start/UnityConfig.cs
@@ -0,0 +1,28 @@
+using System.Web.Http;
+using System.Web.Mvc;
+using Unity;
+using Unity.Mvc5;
+using Mvc5Resolver = Unity.Mvc5.UnityDependencyResolver;
+using ApiResolver = Unity.WebApi.UnityDependencyResolver;
+
+
+namespace AirBench
+{
+    public static class UnityConfig
+    {
+        public static void RegisterComponents()
+        {
+			var container = new UnityContainer();
+
+            // register all your components with the container here
+            // it is NOT necessary to register your controllers
+
+            // e.g. container.RegisterType<ITestService, TestService>();
+
+        // e.g. container.RegisterType<ITestService, TestService>();
+
+        DependencyResolver.SetResolver(new Mvc5Resolver(container));
+            GlobalConfiguration.Configuration.DependencyResolver = new ApiResolver(container);
+        }
+    }
+}
