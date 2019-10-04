f199e2e49b94f061d683ade7ddb14ab01b0437cf
Jason Lu <jasonluX13@gmail.com>
2019-10-04 15:53:22 -0400

    Disabled submit buttons if all form fields are not filled out

---
+++ AirBench/Scripts/login.js
@@ -0,0 +1,20 @@
+﻿let toggleButton = function () {
+   
+    const email = document.querySelector('#email');
+    const password = document.querySelector('#password');
+    const submitButton = document.querySelector('#submit');
+
+    if (email.value === '' || password.value === '') {
+        submitButton.disabled = true;
+    }
+    else {
+        submitButton.disabled = false;
+    }
+};
+
+document.querySelector('#submit').disabled = true;
+const form = document.getElementById('login-form');
+form.addEventListener('keyup', toggleButton);
+
+
+
